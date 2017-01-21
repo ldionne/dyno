c82551d543dda9af1cb0ea72fe1b6e64e1a1714e
Louis Dionne <ldionne.2@gmail.com>
2017-01-21 12:42:01 -0800

    Setup benchmarks using GoogleBenchmark and start using Conan

--- benchmark/any_iterator.cpp
+++ benchmark/any_iterator.cpp
@@ -4,19 +4,82 @@
 
 #include "../example/any_iterator.hpp"
 
+#include <benchmark/benchmark.h>
+
 #include <iterator>
+#include <memory>
 #include <vector>
 
 
-volatile long long COUNTER = 100000000;
+template <typename Value, typename Reference = Value&>
+struct handrolled_iterator {
+  using value_type = Value;
+  using reference = Reference;
 
-int main() {
-  using Iterator = any_iterator<int, std::random_access_iterator_tag>;
-  std::vector<int> input; input.resize(COUNTER);
-  std::vector<int> result; result.reserve(COUNTER);
+  template <typename Iterator>
+  explicit handrolled_iterator(Iterator it)
+    : it_(std::make_unique<model_t<Iterator>>(it))
+  { }
 
-  Iterator first{input.begin()}, last{input.end()};
-  for (; first != last; ++first) {
-    result.push_back(*first);
+  friend bool operator==(handrolled_iterator const& a, handrolled_iterator const& b) {
+    return a.it_->equal(*b.it_);
+  }
+
+  friend bool operator!=(handrolled_iterator const& a, handrolled_iterator const& b) {
+    return !(a == b);
+  }
+
+  handrolled_iterator& operator++() {
+    it_->increment();
+    return *this;
+  }
+
+  Reference operator*() {
+    return it_->dereference();
+  }
+
+private:
+  struct concept_t {
+    virtual void increment() = 0;
+    virtual reference dereference() = 0;
+    virtual bool equal(concept_t const&) const = 0;
+  };
+
+  template <typename Iterator>
+  struct model_t : concept_t {
+    explicit model_t(Iterator it) : it_(it) { }
+    virtual void increment() override final { ++it_; }
+    virtual reference dereference() override final { return *it_; }
+    virtual bool equal(concept_t const& other_) const override final {
+      model_t const& other = static_cast<model_t const&>(other_);
+      return it_ == other.it_;
+    }
+    Iterator it_;
+  };
+
+  std::unique_ptr<concept_t> it_;
+};
+
+
+template <typename Iterator>
+static void BM_iterator(benchmark::State& state) {
+  using T = typename Iterator::value_type;
+  std::vector<T> input(state.range(0)), output(state.range(0));
+
+  while (state.KeepRunning()) {
+
+    state.PauseTiming();
+    Iterator first{input.begin()}, last{input.end()};
+    Iterator result{output.begin()};
+
+    state.ResumeTiming();
+    for (; first != last; ++first, ++result) {
+      *result = *first;
+    }
   }
 }
+
+BENCHMARK_TEMPLATE(BM_iterator, std::vector<int>::iterator)->Arg(100);
+BENCHMARK_TEMPLATE(BM_iterator, any_iterator<int, std::random_access_iterator_tag>)->Arg(100);
+BENCHMARK_TEMPLATE(BM_iterator, handrolled_iterator<int>)->Arg(100);
+BENCHMARK_MAIN();
--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -2,6 +2,16 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
+# This file must define the following variables, each of which should expand
+# to a target name that can be linked against:
+#   DEPENDENCY_hana
+#   DEPENDENCY_callable_traits
+#   DEPENDENCY_google_benchmark
+#
+# Since C++ lacks a standardized or even agreed-upon dependency management
+# system, this makes it easy to use this library with any system by simply
+# providing the dependencies here.
+
 include(ExternalProject)
 ExternalProject_Add(install-Hana EXCLUDE_FROM_ALL 1
     URL https://github.com/boostorg/hana/archive/develop.zip
@@ -17,6 +27,7 @@ ExternalProject_Get_Property(install-Hana SOURCE_DIR)
 add_library(hana INTERFACE)
 target_include_directories(hana INTERFACE ${SOURCE_DIR}/include)
 add_dependencies(hana install-Hana)
+set(DEPENDENCY_hana hana)
 
 ExternalProject_Add(install-CallableTraits EXCLUDE_FROM_ALL 1
     URL https://github.com/badair/callable_traits/archive/master.zip
@@ -32,3 +43,8 @@ ExternalProject_Get_Property(install-CallableTraits SOURCE_DIR)
 add_library(callable_traits INTERFACE)
 target_include_directories(callable_traits INTERFACE ${SOURCE_DIR}/include)
 add_dependencies(callable_traits install-CallableTraits)
+set(DEPENDENCY_callable_traits callable_traits)
+
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_define_targets()
+set(DEPENDENCY_google_benchmark CONAN_PKG::google-benchmark)
