29acb8016ec89cdf63049751c15cf8a2a8337909
Louis Dionne <ldionne.2@gmail.com>
2017-01-21 16:13:36 -0800

    Improve benchmarks for any_iterator and add generic benchmarks

--- benchmark/any_iterator.cpp
+++ benchmark/any_iterator.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
-#include "../example/any_iterator.hpp"
+#include <te.hpp>
 
 #include <benchmark/benchmark.h>
 
@@ -11,58 +11,237 @@
 #include <vector>
 
 
-template <typename Value, typename Reference = Value&>
-struct handrolled_iterator {
-  using value_type = Value;
-  using reference = Reference;
+namespace classic {
+  template <typename Value, typename Reference = Value&>
+  struct any_iterator {
+    using value_type = Value;
+    using reference = Reference;
 
-  template <typename Iterator>
-  explicit handrolled_iterator(Iterator it)
-    : it_(std::make_unique<model_t<Iterator>>(it))
-  { }
+    template <typename Iterator>
+    explicit any_iterator(Iterator it)
+      : it_{std::make_shared<model_t<Iterator>>(it)}
+    { }
 
-  friend bool operator==(handrolled_iterator const& a, handrolled_iterator const& b) {
-    return a.it_->equal(*b.it_);
-  }
-
-  friend bool operator!=(handrolled_iterator const& a, handrolled_iterator const& b) {
-    return !(a == b);
-  }
-
-  handrolled_iterator& operator++() {
-    it_->increment();
-    return *this;
-  }
-
-  Reference operator*() {
-    return it_->dereference();
-  }
-
-private:
-  struct concept_t {
-    virtual void increment() = 0;
-    virtual reference dereference() = 0;
-    virtual bool equal(concept_t const&) const = 0;
-  };
-
-  template <typename Iterator>
-  struct model_t : concept_t {
-    explicit model_t(Iterator it) : it_(it) { }
-    virtual void increment() override final { ++it_; }
-    virtual reference dereference() override final { return *it_; }
-    virtual bool equal(concept_t const& other_) const override final {
-      model_t const& other = static_cast<model_t const&>(other_);
-      return it_ == other.it_;
+    friend bool operator==(any_iterator const& a, any_iterator const& b) {
+      return a.it_->equal(*b.it_);
     }
-    Iterator it_;
+
+    any_iterator& operator++() {
+      it_->increment();
+      return *this;
+    }
+
+    Reference operator*() {
+      return it_->dereference();
+    }
+
+  private:
+    struct concept_t {
+      virtual void increment() = 0;
+      virtual reference dereference() = 0;
+      virtual bool equal(concept_t const&) const = 0;
+    };
+
+    template <typename Iterator>
+    struct model_t : concept_t {
+      explicit model_t(Iterator it) : it_(it) { }
+      void increment() override final { ++it_; }
+      reference dereference() override final { return *it_; }
+      bool equal(concept_t const& other_) const override final {
+        model_t const& other = static_cast<model_t const&>(other_);
+        return it_ == other.it_;
+      }
+      Iterator it_;
+    };
+
+    std::shared_ptr<concept_t> it_;
+  };
+} // end namespace classic
+
+namespace handrolled_split_ptr {
+  template <typename Reference>
+  struct VTable {
+    void (*increment)(void*);
+    Reference (*dereference)(void*);
+    bool (*equal)(void const*, void const*);
   };
 
-  std::unique_ptr<concept_t> it_;
-};
+  template <typename T>
+  void increment(void* self) {
+    ++*static_cast<T*>(self);
+  }
+
+  template <typename T>
+  bool equal(void const* a, void const* b) {
+    return *static_cast<T const*>(a) == *static_cast<T const*>(b);
+  }
+
+  template <typename T>
+  typename T::reference dereference(void* self) {
+    return **static_cast<T*>(self);
+  }
+
+  template <typename T>
+  static constexpr VTable<typename T::reference> const vtable = {
+    &increment<T>, &dereference<T>, &equal<T>
+  };
+
+
+  template <typename Value, typename Reference = Value&>
+  struct any_iterator {
+    using value_type = Value;
+    using reference = Reference;
+
+    template <typename Iterator>
+    explicit any_iterator(Iterator it)
+      : vptr_{&vtable<Iterator>}
+      , self_{std::make_shared<Iterator>(std::move(it))}
+    { }
+
+    any_iterator& operator++() {
+      vptr_->increment(self_.get());
+      return *this;
+    }
+
+    reference operator*() {
+      return vptr_->dereference(self_.get());
+    }
+
+    friend bool operator==(any_iterator const& a, any_iterator const& b) {
+      return a.vptr_->equal(a.self_.get(), b.self_.get());
+    }
+
+  private:
+    VTable<reference> const* vptr_;
+    std::shared_ptr<void> self_;
+  };
+} // end namespace handrolled_split_ptr
+
+namespace handrolled_classic {
+  template <typename Reference>
+  struct VTable {
+    void (*increment)(void*);
+    Reference (*dereference)(void*);
+    bool (*equal)(void const*, void const*);
+  };
+
+  template <typename T>
+  void increment(void* self) {
+    ++*static_cast<T*>(self);
+  }
+
+  template <typename T>
+  bool equal(void const* a, void const* b) {
+    return *static_cast<T const*>(a) == *static_cast<T const*>(b);
+  }
+
+  template <typename T>
+  typename T::reference dereference(void* self) {
+    return **static_cast<T*>(self);
+  }
+
+  template <typename T>
+  static constexpr VTable<typename T::reference> const vtable = {
+    &increment<T>, &dereference<T>, &equal<T>
+  };
+
+
+  template <typename Value, typename Reference = Value&>
+  struct any_iterator {
+    using value_type = Value;
+    using reference = Reference;
+
+    template <typename Iterator>
+    explicit any_iterator(Iterator it)
+      : self_{std::make_shared<model_t<Iterator>>(std::move(it))}
+    { }
+
+    any_iterator& operator++() {
+      self_->vptr_->increment(self_.get());
+      return *this;
+    }
+
+    reference operator*() {
+      return self_->vptr_->dereference(self_.get());
+    }
+
+    friend bool operator==(any_iterator const& a, any_iterator const& b) {
+      return a.self_->vptr_->equal(a.self_.get(), b.self_.get());
+    }
+
+  private:
+    struct concept_t {
+      VTable<reference> const* vptr_;
+    };
+
+    template <typename Iterator>
+    struct model_t : concept_t {
+      using value_type = Value;
+      using reference = Reference;
+      explicit model_t(Iterator it) : concept_t{&vtable<model_t>}, it_{std::move(it)} { }
+      void operator++() { ++it_; }
+      reference operator*() { return *it_; }
+      friend bool operator==(model_t const& a, model_t const& b)
+      { return a.it_ == b.it_; }
+      Iterator it_;
+    };
+
+    std::shared_ptr<concept_t> self_;
+  };
+} // end namespace handrolled_classic
+
+namespace te_style {
+  using namespace te::literals;
+
+  template <typename Reference>
+  constexpr auto Iterator = te::requires(
+    "increment"_s = te::function<void (te::T&)>,
+    "dereference"_s = te::function<Reference (te::T&)>,
+    "equal"_s = te::function<bool (te::T const&, te::T const&)>
+  );
+
+  template <typename T>
+  static te::vtable<decltype(Iterator<typename T::reference>)> const vtable{
+    te::make_concept_map<decltype(Iterator<typename T::reference>)>(
+      "increment"_s = [](T& self) { ++self; },
+      "dereference"_s = [](T& self) -> decltype(auto) { return *self; },
+      "equal"_s = [](T const& a, T const& b) -> bool { return a == b; }
+    )
+  };
+
+  template <typename Value, typename Reference = Value&>
+  struct any_iterator {
+    using value_type = Value;
+    using reference = Reference;
+
+    template <typename Iterator>
+    explicit any_iterator(Iterator it)
+      : vptr_{&vtable<Iterator>}
+      , self_{std::make_shared<Iterator>(std::move(it))}
+    { }
+
+    any_iterator& operator++() {
+      (*vptr_)["increment"_s](self_.get());
+      return *this;
+    }
+
+    reference operator*() {
+      return (*vptr_)["dereference"_s](self_.get());
+    }
+
+    friend bool operator==(any_iterator const& a, any_iterator const& b) {
+      return (*a.vptr_)["equal"_s](a.self_.get(), b.self_.get());
+    }
+
+  private:
+    te::vtable<decltype(Iterator<reference>)> const* vptr_;
+    std::shared_ptr<void> self_;
+  };
+} // end namespace te_style
 
 
 template <typename Iterator>
-static void BM_iterator(benchmark::State& state) {
+static void BM_any_iterator(benchmark::State& state) {
   using T = typename Iterator::value_type;
   std::vector<T> input(state.range(0)), output(state.range(0));
 
@@ -73,13 +252,16 @@ static void BM_iterator(benchmark::State& state) {
     Iterator result{output.begin()};
 
     state.ResumeTiming();
-    for (; first != last; ++first, ++result) {
+    for (; !(first == last); ++first, ++result) {
       *result = *first;
     }
   }
 }
 
-BENCHMARK_TEMPLATE(BM_iterator, std::vector<int>::iterator)->Arg(100);
-BENCHMARK_TEMPLATE(BM_iterator, any_iterator<int, std::random_access_iterator_tag>)->Arg(100);
-BENCHMARK_TEMPLATE(BM_iterator, handrolled_iterator<int>)->Arg(100);
+static constexpr int N = 100;
+BENCHMARK_TEMPLATE(BM_any_iterator, std::vector<int>::iterator)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_iterator, te_style::any_iterator<int>)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_iterator, classic::any_iterator<int>)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_iterator, handrolled_split_ptr::any_iterator<int>)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_iterator, handrolled_classic::any_iterator<int>)->Arg(N);
 BENCHMARK_MAIN();
---
+++ benchmark/n_functions.cpp
@@ -0,0 +1,160 @@
+// Copyright Louis Dionne 2017
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#include <benchmark/benchmark.h>
+
+
+namespace handrolled_classic {
+  struct VTable {
+    void (*f1)(void*);
+    void (*f2)(void*);
+    void (*f3)(void*);
+  };
+
+  template <typename T> void f1(void* self) { static_cast<T*>(self)->f1(); }
+  template <typename T> void f2(void* self) { static_cast<T*>(self)->f2(); }
+  template <typename T> void f3(void* self) { static_cast<T*>(self)->f3(); }
+
+  template <typename T>
+  static constexpr VTable const vtable = {&f1<T>, &f2<T>, &f3<T>};
+
+  struct any {
+    template <typename T>
+    explicit any(T t) : self_{new model_t<T>{t}} { }
+    any& f1() { self_->vptr_->f1(self_); return *this; }
+    any& f2() { self_->vptr_->f2(self_); return *this; }
+    any& f3() { self_->vptr_->f3(self_); return *this; }
+
+  private:
+    struct concept_t {
+      VTable const* vptr_;
+    };
+
+    template <typename T>
+    struct model_t : concept_t {
+      explicit model_t(T t) : concept_t{&vtable<model_t>}, value_{t} { }
+      void f1() { ++value_; };
+      void f2() { --value_; };
+      void f3() { ++value_; };
+      T value_;
+    };
+
+    concept_t* self_;
+  };
+} // end namespace handrolled_classic
+
+namespace handrolled_split_ptr {
+  struct VTable {
+    void (*f1)(void*);
+    void (*f2)(void*);
+    void (*f3)(void*);
+  };
+
+  template <typename T> void f1(void* self) { static_cast<T*>(self)->f1(); }
+  template <typename T> void f2(void* self) { static_cast<T*>(self)->f2(); }
+  template <typename T> void f3(void* self) { static_cast<T*>(self)->f3(); }
+
+  template <typename T>
+  static constexpr VTable const vtable = {&f1<T>, &f2<T>, &f3<T>};
+
+  struct any {
+    template <typename T>
+    explicit any(T t) : vptr_{&vtable<model_t<T>>}, self_{new model_t<T>{t}} { }
+    any& f1() { vptr_->f1(self_); return *this; }
+    any& f2() { vptr_->f2(self_); return *this; }
+    any& f3() { vptr_->f3(self_); return *this; }
+
+  private:
+    template <typename T>
+    struct model_t {
+      explicit model_t(T t) : value_{t} { }
+      void f1() { ++value_; };
+      void f2() { --value_; };
+      void f3() { ++value_; };
+      T value_;
+    };
+
+    VTable const* vptr_;
+    void* self_;
+  };
+} // end namespace handrolled_split_ptr
+
+namespace classic {
+  struct any {
+    template <typename T>
+    explicit any(T t) : self_{new model_t<T>{t}} { }
+    any& f1() { self_->f1(); return *this; }
+    any& f2() { self_->f2(); return *this; }
+    any& f3() { self_->f3(); return *this; }
+
+  private:
+    struct concept_t {
+      virtual void f1() = 0;
+      virtual void f2() = 0;
+      virtual void f3() = 0;
+    };
+
+    template <typename T>
+    struct model_t : concept_t {
+      explicit model_t(T t) : value_{t} { }
+      void f1() override final { ++value_; }
+      void f2() override final { --value_; }
+      void f3() override final { ++value_; }
+      T value_;
+    };
+
+    concept_t* self_;
+  };
+} // end namespace classic
+
+template <typename Any>
+static void BM_any_1_function(benchmark::State& state) {
+  Any a{0};
+  int const N = state.range(0);
+  while (state.KeepRunning()) {
+    for (int i = 0; i != N; ++i) {
+      benchmark::DoNotOptimize(a.f1());
+    }
+  }
+}
+
+template <typename Any>
+static void BM_any_2_function(benchmark::State& state) {
+  Any a{0};
+  int const N = state.range(0);
+  while (state.KeepRunning()) {
+    for (int i = 0; i != N; ++i) {
+      benchmark::DoNotOptimize(a.f1());
+      benchmark::DoNotOptimize(a.f2());
+    }
+  }
+}
+
+template <typename Any>
+static void BM_any_3_function(benchmark::State& state) {
+  Any a{0};
+  int const N = state.range(0);
+  while (state.KeepRunning()) {
+    for (int i = 0; i != N; ++i) {
+      benchmark::DoNotOptimize(a.f1());
+      benchmark::DoNotOptimize(a.f2());
+      benchmark::DoNotOptimize(a.f3());
+    }
+  }
+}
+
+static constexpr int N = 1000;
+
+BENCHMARK_TEMPLATE(BM_any_1_function, handrolled_classic::any)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_2_function, handrolled_classic::any)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_3_function, handrolled_classic::any)->Arg(N);
+
+BENCHMARK_TEMPLATE(BM_any_1_function, handrolled_split_ptr::any)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_2_function, handrolled_split_ptr::any)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_3_function, handrolled_split_ptr::any)->Arg(N);
+
+BENCHMARK_TEMPLATE(BM_any_1_function, classic::any)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_2_function, classic::any)->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_3_function, classic::any)->Arg(N);
+BENCHMARK_MAIN();
