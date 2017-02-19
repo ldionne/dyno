ccf4272a1b8c2947fbc2fe1019020e5fefe7a82b
Louis Dionne <ldionne.2@gmail.com>
2017-02-19 14:16:35 -0800

    [benchmark] Increase the n_functions benchmark

--- benchmark/n_functions.cpp
+++ benchmark/n_functions.cpp
@@ -12,14 +12,18 @@ namespace { namespace handrolled_classic {
     void (*f1)(void*);
     void (*f2)(void*);
     void (*f3)(void*);
+    void (*f4)(void*);
+    void (*f5)(void*);
   };
 
   template <typename T> void f1(void* self) { static_cast<T*>(self)->f1(); }
   template <typename T> void f2(void* self) { static_cast<T*>(self)->f2(); }
   template <typename T> void f3(void* self) { static_cast<T*>(self)->f3(); }
+  template <typename T> void f4(void* self) { static_cast<T*>(self)->f4(); }
+  template <typename T> void f5(void* self) { static_cast<T*>(self)->f5(); }
 
   template <typename T>
-  static constexpr VTable const vtable = {&f1<T>, &f2<T>, &f3<T>};
+  static constexpr VTable const vtable = {&f1<T>, &f2<T>, &f3<T>, &f4<T>, &f5<T>};
 
   struct any {
     template <typename T>
@@ -27,6 +31,8 @@ namespace { namespace handrolled_classic {
     any& f1() { self_->vptr_->f1(self_); return *this; }
     any& f2() { self_->vptr_->f2(self_); return *this; }
     any& f3() { self_->vptr_->f3(self_); return *this; }
+    any& f4() { self_->vptr_->f4(self_); return *this; }
+    any& f5() { self_->vptr_->f5(self_); return *this; }
 
   private:
     struct concept_t {
@@ -39,6 +45,8 @@ namespace { namespace handrolled_classic {
       void f1() { ++value_; };
       void f2() { --value_; };
       void f3() { ++value_; };
+      void f4() { --value_; };
+      void f5() { ++value_; };
       T value_;
     };
 
@@ -51,14 +59,18 @@ namespace { namespace handrolled_split_ptr {
     void (*f1)(void*);
     void (*f2)(void*);
     void (*f3)(void*);
+    void (*f4)(void*);
+    void (*f5)(void*);
   };
 
   template <typename T> void f1(void* self) { static_cast<T*>(self)->f1(); }
   template <typename T> void f2(void* self) { static_cast<T*>(self)->f2(); }
   template <typename T> void f3(void* self) { static_cast<T*>(self)->f3(); }
+  template <typename T> void f4(void* self) { static_cast<T*>(self)->f4(); }
+  template <typename T> void f5(void* self) { static_cast<T*>(self)->f5(); }
 
   template <typename T>
-  static constexpr VTable const vtable = {&f1<T>, &f2<T>, &f3<T>};
+  static constexpr VTable const vtable = {&f1<T>, &f2<T>, &f3<T>, &f4<T>, &f5<T>};
 
   struct any {
     template <typename T>
@@ -66,6 +78,8 @@ namespace { namespace handrolled_split_ptr {
     any& f1() { vptr_->f1(self_); return *this; }
     any& f2() { vptr_->f2(self_); return *this; }
     any& f3() { vptr_->f3(self_); return *this; }
+    any& f4() { vptr_->f4(self_); return *this; }
+    any& f5() { vptr_->f5(self_); return *this; }
 
   private:
     template <typename T>
@@ -74,6 +88,8 @@ namespace { namespace handrolled_split_ptr {
       void f1() { ++value_; };
       void f2() { --value_; };
       void f3() { ++value_; };
+      void f4() { --value_; };
+      void f5() { ++value_; };
       T value_;
     };
 
@@ -89,12 +105,16 @@ namespace { namespace classic {
     any& f1() { self_->f1(); return *this; }
     any& f2() { self_->f2(); return *this; }
     any& f3() { self_->f3(); return *this; }
+    any& f4() { self_->f4(); return *this; }
+    any& f5() { self_->f5(); return *this; }
 
   private:
     struct concept_t {
       virtual void f1() = 0;
       virtual void f2() = 0;
       virtual void f3() = 0;
+      virtual void f4() = 0;
+      virtual void f5() = 0;
     };
 
     template <typename T>
@@ -103,6 +123,8 @@ namespace { namespace classic {
       void f1() override final { ++value_; }
       void f2() override final { --value_; }
       void f3() override final { ++value_; }
+      void f4() override final { --value_; }
+      void f5() override final { ++value_; }
       T value_;
     };
 
@@ -116,7 +138,9 @@ namespace { namespace te_split_ptr {
   struct Concept : decltype(te::requires(
     "f1"_s = te::function<void (te::T&)>,
     "f2"_s = te::function<void (te::T&)>,
-    "f3"_s = te::function<void (te::T&)>
+    "f3"_s = te::function<void (te::T&)>,
+    "f4"_s = te::function<void (te::T&)>,
+    "f5"_s = te::function<void (te::T&)>
   )) { };
 
   struct any {
@@ -126,7 +150,9 @@ namespace { namespace te_split_ptr {
         te::make_concept_map<Concept, T>(
           "f1"_s = [](T& self) { ++self; },
           "f2"_s = [](T& self) { --self; },
-          "f3"_s = [](T& self) { ++self; }
+          "f3"_s = [](T& self) { ++self; },
+          "f4"_s = [](T& self) { --self; },
+          "f5"_s = [](T& self) { ++self; }
         )
       }
       , self_{new T(t)}
@@ -135,6 +161,8 @@ namespace { namespace te_split_ptr {
     any& f1() { vtable_["f1"_s](self_); return *this; }
     any& f2() { vtable_["f2"_s](self_); return *this; }
     any& f3() { vtable_["f3"_s](self_); return *this; }
+    any& f4() { vtable_["f4"_s](self_); return *this; }
+    any& f5() { vtable_["f5"_s](self_); return *this; }
 
   private:
     te::remote_vtable<te::local_vtable<Concept>> vtable_;
@@ -178,20 +206,59 @@ static void BM_any_3_function(benchmark::State& state) {
   }
 }
 
-static constexpr int N = 1000;
+template <typename Any>
+static void BM_any_4_function(benchmark::State& state) {
+  Any a{0};
+  int const N = state.range(0);
+  while (state.KeepRunning()) {
+    for (int i = 0; i != N; ++i) {
+      benchmark::DoNotOptimize(a.f1());
+      benchmark::DoNotOptimize(a.f2());
+      benchmark::DoNotOptimize(a.f3());
+      benchmark::DoNotOptimize(a.f4());
+    }
+  }
+}
 
-BENCHMARK_TEMPLATE(BM_any_1_function, handrolled_classic::any   )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_1_function, handrolled_split_ptr::any )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_1_function, classic::any              )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_1_function, te_split_ptr::any         )->Arg(N);
+template <typename Any>
+static void BM_any_5_function(benchmark::State& state) {
+  Any a{0};
+  int const N = state.range(0);
+  while (state.KeepRunning()) {
+    for (int i = 0; i != N; ++i) {
+      benchmark::DoNotOptimize(a.f1());
+      benchmark::DoNotOptimize(a.f2());
+      benchmark::DoNotOptimize(a.f3());
+      benchmark::DoNotOptimize(a.f4());
+      benchmark::DoNotOptimize(a.f5());
+    }
+  }
+}
 
-BENCHMARK_TEMPLATE(BM_any_2_function, handrolled_classic::any   )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_2_function, handrolled_split_ptr::any )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_2_function, classic::any              )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_2_function, te_split_ptr::any         )->Arg(N);
+static constexpr int N = 100;
 
-BENCHMARK_TEMPLATE(BM_any_3_function, handrolled_classic::any   )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_3_function, handrolled_split_ptr::any )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_3_function, classic::any              )->Arg(N);
-BENCHMARK_TEMPLATE(BM_any_3_function, te_split_ptr::any         )->Arg(N);
+BENCHMARK_TEMPLATE(BM_any_1_function, handrolled_classic::any   )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_1_function, handrolled_split_ptr::any )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_1_function, classic::any              )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_1_function, te_split_ptr::any         )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+
+BENCHMARK_TEMPLATE(BM_any_2_function, handrolled_classic::any   )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_2_function, handrolled_split_ptr::any )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_2_function, classic::any              )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_2_function, te_split_ptr::any         )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+
+BENCHMARK_TEMPLATE(BM_any_3_function, handrolled_classic::any   )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_3_function, handrolled_split_ptr::any )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_3_function, classic::any              )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_3_function, te_split_ptr::any         )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+
+BENCHMARK_TEMPLATE(BM_any_4_function, handrolled_classic::any   )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_4_function, handrolled_split_ptr::any )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_4_function, classic::any              )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_4_function, te_split_ptr::any         )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+
+BENCHMARK_TEMPLATE(BM_any_5_function, handrolled_classic::any   )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_5_function, handrolled_split_ptr::any )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_5_function, classic::any              )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(BM_any_5_function, te_split_ptr::any         )->Arg(N)->Repetitions(4)->ReportAggregatesOnly(true);
 BENCHMARK_MAIN();
