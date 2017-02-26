b67b458e4428091af0c0ff2bcfae2799ec3e50f1
Louis Dionne <ldionne.2@gmail.com>
2017-02-25 20:34:57 -0800

    [README] Pass custom concept map to poly in overview

--- README.md
+++ README.md
@@ -25,16 +25,12 @@ struct Drawable : decltype(te::requires(
   "draw"_s = te::function<void (te::T const&, std::ostream&)>
 )) { };
 
-// Provide a default implementation of that interface
-template <typename T>
-auto const te::default_concept_map<Drawable, T> = te::make_concept_map(
-  "draw"_s = [](T const& self, std::ostream& out) { self.draw(out); }
-);
-
 // Define an object that can hold anything that can be drawn.
 struct drawable {
   template <typename T>
-  drawable(T x) : poly_{x} { }
+  drawable(T x) : poly_{x, te::make_concept_map(
+    "draw"_s = [](T const& self, std::ostream& out) { self.draw(out); }
+  )} { }
 
   void draw(std::ostream& out) const
   { poly_.virtual_("draw"_s)(poly_.get(), out); }
@@ -44,11 +40,11 @@ private:
 };
 
 struct Square {
-  void draw(std::ostream& out) const { /* ... */ }
+  void draw(std::ostream& out) const { out << "Square"; }
 };
 
 struct Circle {
-  void draw(std::ostream& out) const { /* ... */ }
+  void draw(std::ostream& out) const { out << "Circle"; }
 };
 
 void f(drawable const& d) {
--- example/overview.cpp
+++ example/overview.cpp
@@ -2,6 +2,9 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
+#include "../test/testing.hpp"
+#include <sstream>
+
 //////////////////////////////////////////////////////////////////////////////
 // Important: Keep this file in sync with the Overview in the README
 //////////////////////////////////////////////////////////////////////////////
@@ -14,16 +17,12 @@ struct Drawable : decltype(te::requires(
   "draw"_s = te::function<void (te::T const&, std::ostream&)>
 )) { };
 
-// Provide a default implementation of that interface
-template <typename T>
-auto const te::default_concept_map<Drawable, T> = te::make_concept_map(
-  "draw"_s = [](T const& self, std::ostream& out) { self.draw(out); }
-);
-
 // Define an object that can hold anything that can be drawn.
 struct drawable {
   template <typename T>
-  drawable(T x) : poly_{x} { }
+  drawable(T x) : poly_{x, te::make_concept_map(
+    "draw"_s = [](T const& self, std::ostream& out) { self.draw(out); }
+  )} { }
 
   void draw(std::ostream& out) const
   { poly_.virtual_("draw"_s)(poly_.get(), out); }
@@ -33,11 +32,11 @@ private:
 };
 
 struct Square {
-  void draw(std::ostream& out) const { /* ... */ }
+  void draw(std::ostream& out) const { out << "Square"; }
 };
 
 struct Circle {
-  void draw(std::ostream& out) const { /* ... */ }
+  void draw(std::ostream& out) const { out << "Circle"; }
 };
 
 void f(drawable const& d) {
@@ -46,6 +45,13 @@ void f(drawable const& d) {
 //////////////////////////////////////////////////////////////////////////////
 
 int main() {
+  std::stringstream out;
+  std::cout.rdbuf(out.rdbuf());
+
   f(Square{});
+  TE_CHECK(out.str() == "Square");
+  out.str("");
+
   f(Circle{});
+  TE_CHECK(out.str() == "Circle");
 }
