29da339d24532545f81784609d2fd8d041cc3db9
Louis Dionne <ldionne.2@gmail.com>
2017-03-05 18:37:36 -0800

    [vtable] Implement iterative vtable policy resolution
    Policies that come earlier in the `dyno::vtable<...>` list will now remove the
    functions they take from the list of functions to be included in the vtable.

---
+++ test/vtable.fail.5.cpp
@@ -0,0 +1,30 @@
+// Copyright Louis Dionne 2017
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#include <dyno/concept.hpp>
+#include <dyno/concept_map.hpp>
+#include <dyno/vtable.hpp>
+using namespace dyno::literals;
+
+
+struct Concept : decltype(dyno::requires(
+  "f"_s = dyno::function<void (dyno::T const&)>
+)) { };
+
+struct Foo { };
+
+template <>
+auto const dyno::concept_map<Concept, Foo> = dyno::make_concept_map(
+  "f"_s = [](Foo&) { }
+);
+
+int main() {
+  auto complete = dyno::complete_concept_map<Concept, Foo>(dyno::concept_map<Concept, Foo>);
+
+  // MESSAGE[Some functions specified in this selector are not part of the concept to which the selector was applied]
+  dyno::vtable<
+    dyno::local<dyno::except<decltype("nonexistent"_s)>>,
+    dyno::remote<dyno::everything_else>
+  >::apply<Concept> vtable{complete};
+}
