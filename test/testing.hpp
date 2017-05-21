df670193a572370a1ed885a89debd2bb85630449
Louis Dionne <ldionne.2@gmail.com>
2017-05-21 15:29:05 -0700

    [test] Fix DYNO_CHECK macro so it works in Release mode too

--- test/testing.hpp
+++ test/testing.hpp
@@ -5,9 +5,16 @@
 #ifndef DYNO_TEST_TESTING_HPP
 #define DYNO_TEST_TESTING_HPP
 
-#include <cassert>
+#include <cstdio>
+#include <cstdlib>
 
 
-#define DYNO_CHECK(...) assert((__VA_ARGS__))
+// Definition copied from `assert`. We don't use `assert` because we want to
+// allow testing in Release mode, when `_NDEBUG` is defined.
+#define DYNO_CHECK(...)                                                   \
+  ((__VA_ARGS__) ? (void)0 : DYNO_CHECK_IMPL(#__VA_ARGS__, __FILE__, __LINE__))
+
+#define DYNO_CHECK_IMPL(expr, file, line) \
+  (std::printf("%s:%u: failed assertion `%s'\n", file, line, expr), std::abort())
 
 #endif // DYNO_TEST_TESTING_HPP
