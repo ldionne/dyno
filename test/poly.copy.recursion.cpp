cd4abd61389116e3becc424bf89ae44f60c1b2be
Louis Dionne <ldionne.2@gmail.com>
2017-07-09 16:24:49 -0700

    [WIP] Fix a bug with infinite recursion when making a copy

---
+++ test/poly.copy.recursion.cpp
@@ -0,0 +1,38 @@
+// Copyright Louis Dionne 2017
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#include "testing.hpp"
+
+#include <dyno/builtin.hpp>
+#include <dyno/concept.hpp>
+#include <dyno/poly.hpp>
+
+#include <utility>
+
+
+// This test checks that we do not hit infinite recursion when trying to make
+// a copy of a type-erased wrapper, which once happened.
+
+struct wrapper {
+  template <typename T>
+  wrapper(T&& x)
+    : poly_{std::forward<T>(x)}
+  { }
+
+  wrapper(wrapper const&) = default;
+  wrapper(wrapper&&) = default;
+
+private:
+  dyno::poly<decltype(dyno::requires(
+    dyno::CopyConstructible{}
+  ))> poly_;
+};
+
+struct Foo { };
+
+int main() {
+  wrapper w = Foo{};
+  wrapper implicit_copy = w;
+  wrapper explicit_copy{w};
+}
