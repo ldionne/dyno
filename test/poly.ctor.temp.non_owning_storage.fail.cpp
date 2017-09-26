7e9182101b7189d64578fc86f158269b1a2f941f
Louis Dionne <ldionne.2@gmail.com>
2017-09-26 16:17:09 -0700

    [tests] Add test for constructibility of poly with non-owning storage

---
+++ test/poly.ctor.temp.non_owning_storage.fail.cpp
@@ -0,0 +1,19 @@
+// Copyright Louis Dionne 2017
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#include <dyno/concept.hpp>
+#include <dyno/poly.hpp>
+#include <dyno/storage.hpp>
+
+
+// This test makes sure that we get a compiler error if we try to construct a
+// `dyno::poly` using non-owning storage from a temporary.
+
+struct Concept : decltype(dyno::requires()) { };
+
+struct Foo { };
+
+int main() {
+  dyno::poly<Concept, dyno::non_owning_storage> poly{Foo{}};
+}
