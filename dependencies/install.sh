ab64189db94703d661dfd6bd0e85a6677ebb7981
Louis Dionne <ldionne.2@gmail.com>
2017-12-02 15:43:34 -0800

    [CMake] Make sure the build system works without the optional dependencies
    Fixes #31

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -8,8 +8,15 @@ CMAKE_SOURCE_DIR="${PWD}"
 
 rm -rf "${CMAKE_INSTALL_PREFIX}" "${CMAKE_BINARY_DIR}"
 mkdir -p "${CMAKE_INSTALL_PREFIX}" "${CMAKE_BINARY_DIR}"
-(cd "${CMAKE_BINARY_DIR}" &&
-  cmake "${CMAKE_SOURCE_DIR}" -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" "${@}")
+cd "${CMAKE_BINARY_DIR}"
+
+if [[ $# -ge 0 && "$1" == "--minimal" ]]; then
+  shift
+  cmake "${CMAKE_SOURCE_DIR}" -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" -DMINIMAL="true" "${@}"
+else
+  cmake "${CMAKE_SOURCE_DIR}" -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" "${@}"
+fi
+
 cmake --build "${CMAKE_BINARY_DIR}"
 
 echo "************************************************************************"
